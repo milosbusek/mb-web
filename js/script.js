f105fd070a1090258e65f27e38b2bdb70cd78a0a
milosbusek <milosbusek@gmail.com>
2026-06-08 14:27:10 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menuBtn");
 const nav = document.getElementById("nav");
 
-menuBtn?.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
+if (menuBtn && nav) {
+  menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+}
